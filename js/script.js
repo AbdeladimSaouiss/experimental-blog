8c03d7502acb1f4eb9dceea31e17b978f5fd40aa
Abdeladim Saouiss <abdeladim.saouiss@gmail.com>
2019-01-09 17:45:24 +0100

    Portfolio v0

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// const axios = require('axios');
+var url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=1014d84c0cfa4bb389fa7e71415bc8cc';
+
+// var req = new Request(url);
+
+// fetch(req)
+//     .then(function(res){
+//         return res.json();
+//     }).then(function(data) {
+//         console.log(data.articles);
+//     })
+
+axios.get(url)
+    .then(function(res) {
+        console.log(res.data.articles);
+        res.data.articles.map(article => article)
+    })

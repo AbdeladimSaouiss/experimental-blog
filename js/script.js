8b5f473cb1c824300a8a99ed222007c85e663496
Abdeladim Saouiss <abdeladim.saouiss@gmail.com>
2019-01-14 17:54:09 +0100

    Inserting data comming frim newsAPI in blog page

--- js/script.js
+++ js/script.js
@@ -12,8 +12,39 @@ var url = 'https://newsapi.org/v2/top-headlines?' +
 //         console.log(data.articles);
 //     })
 
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, child) {
+    return parent.appendChild(child);
+}
+
+const cards = document.getElementById("cards") 
 axios.get(url)
     .then(function(res) {
         console.log(res.data.articles);
-        res.data.articles.map(article => article)
+        let articles = res.data.articles;  // Data comes from Url
+        articles.map((article) => {
+            let card = createNode("div"),
+                cardImg = createNode('div'),
+                img = createNode('img'),
+                cardCt = createNode('div'),
+                p = createNode('p');
+
+            //Add classes
+            card.classList.add('card');
+            cardImg.classList.add('card-image');
+            cardCt.classList.add('card-content');
+
+            // Add content
+            img.src = article.urlToImage;
+            p.innerText = article.title; 
+
+            append(cardCt, p);
+            append(cardImg, img);
+            append(card, cardImg);
+            append(card, cardCt);
+            append(cards, card)
+        });
     })
